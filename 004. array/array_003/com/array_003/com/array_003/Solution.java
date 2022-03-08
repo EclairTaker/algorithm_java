9f38579a1ac09ef798bd355bb2ef35d0d938c011
Taker K <kkt829@gmail.com>
2022-03-09 06:18:06 +0900

    task_003
    문제 이해가 너무 어렵다 진짜

--- 004. array/array_003/com/array_003/com/array_003/Solution.java
+++ 004. array/array_003/com/array_003/com/array_003/Solution.java
@@ -1,40 +1,31 @@
 package com.array_003;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 
 public class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        // ArrayList 선언
-        ArrayList<Integer> idx = new ArrayList<Integer>();
-        int cnt = 0;
 
-        // ArrayList로 컨버팅
-        for (Integer cit : citations)   {
-            // 중복값을 제거해서 인용 횟수만 직관적으로 파악
-            if(!idx.contains(cit))
-                idx.add(cit);
-        }
+        // 해당 Array를 오름차순 정렬 후 조건 충족하는 가장 최소값을 구하면 된다.
+        // 어차피 올라갈 수록 인용된 논문의 수는 줄어들 것이기 때문
+        // 문제의 정리 >> h번 이상 인용된 논문이 h편 이상일 경우에 h 최대값을 구하라
+        // 인용 수를 정렬 >> 뒤로 갈 수록 인용 횟수가 논문 편수보다 커지게 된다.
+        Arrays.sort(citations);
 
-        // Collections를 통한 내림차순 수행
-        Collections.sort(idx, Comparator.reverseOrder());
-        System.out.println(idx);
-
-        for (Integer high_rank : idx)   {
-            for (Integer idx_list : citations)  {
-                if (idx_list >= high_rank)  {
-                    cnt ++;
-                }
+        // citations.length = 전체 논문의 개수
+        for (int i =0; i < citations.length; i++)   {
+            // citations.length를 쓰는 이유 : 기준을 만족한 논문의 수는 절대 전체 논문의 양을 넘을 수 없기 때문
+            // 정렬 된 citations는 인용횟수가 가장 적을 때부터 보여줌
+            // i 일 때 성립할 수 있는 기준치 달성한 논문의 수 최대값
+            // 기준점으로 활용할 수 있는 인용 논문 편수는 citations.length 내에서 밖에 나올 수 없음.
+            int h_idx = citations.length - i;
+            // 기준 달성 논문 편수 : h_idx, 인용된 논문 편수 : citations[i]
+            // n편(citations[i]) 이상 인용된 논문의 갯수가 n편(h_idx)이상일 경우 그 최소값
+            if (citations[i] >= h_idx)  {
+                answer = h_idx;
+                break;
             }
-
-            if (cnt >= high_rank)
-                answer = high_rank;
-            else
-                cnt = 0;
         }
-
         return answer;
     }
 }

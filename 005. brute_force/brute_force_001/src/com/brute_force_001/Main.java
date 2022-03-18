ea5c85ac2b46fab687fb96933b8c4c73b03ee49f
Taker K <kkt829@gmail.com>
2022-03-19 00:40:02 +0900

    after covid-19

---
+++ 005. brute_force/brute_force_001/src/com/brute_force_001/Main.java
@@ -0,0 +1,12 @@
+package com.brute_force_001;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] input = {1,2,3,4,5};
+        int[] answer = {};
+
+        answer = sol.solution(input);
+        System.out.println(answer);
+    }
+}
---
+++ 005. brute_force/brute_force_001/src/com/brute_force_001/Solution.java
@@ -0,0 +1,22 @@
+package com.brute_force_001;
+
+public class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        int ans_len = answers.length;
+
+        // 답안을 하나하나 만들고 비교하는 것이 옳은가?
+        // 어차피 답안 생성은 규칙적이다
+        int[] test_001 = {1,2,3,4,5};
+        int[] test_002 = {2,1,2,3,2,4,2,5};
+        int[] test_003 = {3,3,1,1,2,2,4,4,5,5};
+
+
+        // 1번 수포자 : 5번마다 반복
+        // 2번 수포자 : 8번마다 반복
+        // 3번 수포자 : 10번마다 반복
+
+
+        return answer;
+    } 
+}

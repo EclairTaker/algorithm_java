e8b7f3c5c85e40bba57a81ee16da2545bc12687a
Taker K <kkt829@gmail.com>
2022-03-23 05:22:11 +0900

    002 task in progress

---
+++ 005. brute_force/brute_force_001/src/com/brute_force_002/Main.java
@@ -0,0 +1,11 @@
+package com.brute_force_002;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String numbers = "17";
+        
+        answer = sol.solution(numbers);
+        System.out.println(answer);
+    }
+}
---
+++ 005. brute_force/brute_force_001/src/com/brute_force_002/Solution.java
@@ -0,0 +1,17 @@
+package com.brute_force_002;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Solution {
+    public int solution(String numbers) {
+        int answer = 0;
+        // 문제의 이해
+        // 과정 1. 문자열 숫자 조합을 통해 생성할 수 있는 최소~최대의 조합을 확인
+        // 과정 2. 에리토스테네스의 체를 활용한 소수 분별 수행
+
+        
+
+        return answer;
+    }
+}

99c12be04c4853efc682614ffd66ca9a00cef432
Taker K <kkt829@gmail.com>
2022-03-30 04:56:20 +0900

    003 task

---
+++ 005. brute_force/brute_force_001/src/com/brute_force_003/com/brute_force_003/Main.java
@@ -0,0 +1,13 @@
+package com.brute_force_003;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int brown = 10;
+        int yellow = 2;
+        int[] answer = {};
+        
+        answer = sol.solution(brown, yellow);
+        System.out.println(answer);
+    }
+}

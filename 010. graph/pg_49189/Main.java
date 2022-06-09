4e4123bb509abcd644b463eaa44ce8077fc61f4f
Taker K <kkt829@gmail.com>
2022-06-10 01:13:58 +0900

    Update Main.java
    그냥 BFS로 풀이 수행

--- 010. graph/pg_49189/Main.java
+++ 010. graph/pg_49189/Main.java
@@ -1,61 +1,52 @@
 import java.util.*;
 
 public class Main {
-    // 다익스트라 알고리즘 적용하여 새로 풀이 수행
-    // 최대 경우의 수 노드 20,000 * 간선 50,000 ...?
-    static final int INF = 1000000;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        // class Node 사용해서 ArrayList 선언
-        // Class Node의 구조 (index, cost)
-        ArrayList<Node>[] graph = new ArrayList[n];
+        visited = new boolean[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+        for (int i = 0; i <= n; i++)    {
+            graph.add(i, new ArrayList<>());
         }
 
-        for (int[] e : edge)    {
-            // Graph를 작성
-            // Node간 연결선에 weight, distance는 존재하지 않기 때문에 전부 1로 통일 (거리는 동일하기 때문)
-            graph[e[0] - 1].add(new Node(e[1] - 1, 1));
-            graph[e[1] - 1].add(new Node(e[0] - 1, 1));
+        // Graph가 양방향 그래프이다.
+        for (int i = 0; i < edge.length; i++)   {
+            graph.get(edge[i][0]).add(edge[i][1]);
+            graph.get(edge[i][1]).add(edge[i][0]);
         }
-
-        // 최대거리 확인을 위한 distance
-        int[] distance = new int[n];
-        Arrays.fill(distance, INF);
-        // 시작지점이기 때문에 0으로 Start
-        distance[0] = 0;
-        PriorityQueue<Node> queue = new PriorityQueue();
-        queue.add(new Node(0, 0));
-
-        while (!queue.isEmpty())    {
-            // 현재 방문 노드 정보를 확인
-            Node curNode = queue.poll();
-
-            // 해당 노드의 번호를 통해 조회하여 해당 
-            for (int i = 0; i < graph[curNode.index].size(); i++)   {
-                Node next = graph[curNode.index].get(i);
-            }
-        }
-
+        answer = bfs();
         return answer;
     }
 
-    // 사용하기 위한 class Node 선언
-    class Node implements Comparable<Node>  {
-        int index;
-        int cost;
+    public static int bfs()  {
+        Queue<Integer> queue = new LinkedList<>();
+        // 시작지점
+        queue.add(1);
+        visited[1] = true;
 
-        public Node(int index, int cost)    {
-            this.index = index;
-            this.cost = cost;
-        }
+        int cnt = 0;
+        while (true)    {
+            Queue<Integer> temp = new LinkedList<>();
 
-        @Override
-        public int compareTo(Node o)    {
-            return Integer.compare(this.cost, o.cost);
+            while (!queue.isEmpty())    {
+                int cur = queue.poll();
+                for (int adj : graph.get(cur))  {
+                    if (!visited[adj])  {
+                        temp.add(adj);
+                        visited[adj] = true;
+                    }
+                }
+            }
+            if (temp.isEmpty()) {
+                break;
+            }
+            queue.addAll(temp);
+            cnt = temp.size();
         }
+        return cnt;
     }
 }
 
@@ -63,3 +54,4 @@ public class Main {
 
 // 참조 : https://born2bedeveloper.tistory.com/43
 // 참조 : https://arinnh.tistory.com/40
+// 참조 : https://velog.io/@hammii/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B0%80%EC%9E%A5-%EB%A8%BC-%EB%85%B8%EB%93%9C-java
